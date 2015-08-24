c5fd51867d5f8ff8dffff5105f69bb978181a052
Sylvain HENRY <hsyl20@gmail.com>
2015-08-24 23:01:13 +0200

    Basic tracer

--- src/lib/Graphics/SunBurn/Objects.hs
+++ src/lib/Graphics/SunBurn/Objects.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Graphics.SunBurn.Objects
    ( Hit (..)
    , Ray (..)
@@ -6,9 +8,16 @@ module Graphics.SunBurn.Objects
    , Plane(..)
    , Sphere(..)
    , Hittable (..)
+   , Object(..)
+   , ViewPlane(..)
+   , renderScene
+   , Scene(..)
    )
    where
 
+import Data.Maybe (mapMaybe)
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 data Hit = Hit
    { hitPoint  :: Vector3D
@@ -53,7 +62,7 @@ data Ray = Ray
    } deriving (Show)
 
 class Hittable o where
-   hit :: o -> Ray -> Maybe (Double,Hit)
+   hit :: o -> Ray -> Maybe (Double,Hit) -- ^ Indicate if the object is hit by the ray (returns hit info)
 
 
 data Plane = Plane
@@ -98,3 +107,41 @@ instance Hittable Sphere where
 
 
 
+
+data ViewPlane = ViewPlane
+   { vpHorizontalResolution   :: Int
+   , vpVerticalResolution     :: Int
+   , vpPixelSize              :: Double
+   , vpGamma                  :: Float
+   , vpGammaInv               :: Float
+   }
+
+
+data Scene = Scene
+   { sceneObjects          :: [Object]
+   , sceneBackgroundColor  :: Color
+   }
+
+data Object = forall a. Hittable a => Object a
+
+-- | Trace the ray and find the first hit (TODO: should return a colorable object)
+traceMin :: [Object] -> Ray -> Maybe (Double,Hit)
+traceMin os ray = headMaybe . sortBy (comparing fst) . mapMaybe hit' $ os
+   where
+      headMaybe [] = Nothing
+      headMaybe (x:_) = Just x
+      hit' (Object o) = hit o ray
+
+
+renderScene :: Scene -> ViewPlane -> Double -> [[Color]]
+renderScene scn vp zw = 
+      fmap trace <$> [ [ (c,r) | c <- [0 .. vpHorizontalResolution vp - 1]] | r <- [0 .. vpVerticalResolution vp - 1]]
+   where
+      makeRay c r = Ray (Vector3D x y zw) (Vector3D 0.0 0.0 (-1.0))
+         where
+            x = vpPixelSize vp * (fromIntegral c - 0.5 * (fromIntegral (vpHorizontalResolution vp) - 1.0))
+            y = vpPixelSize vp * (fromIntegral r - 0.5 * (fromIntegral (vpVerticalResolution   vp) - 1.0))
+
+      trace (c,r) = case traceMin (sceneObjects scn) (makeRay c r) of
+         Nothing -> sceneBackgroundColor scn
+         Just _  -> RGBColor 255 0 0
